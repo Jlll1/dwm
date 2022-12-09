c7fa4ab02f41eb6906916da513154e5ee75c66ac
Jlll1 <arghantentua@tutanota.com>
2022-12-09 23:42:29 +0100

    Add xrdb patch

--- config.def.h
+++ config.def.h
@@ -11,17 +11,18 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 #define ICONSPACING 5 /* space between icon and title */
 static const char *fonts[]          = { "monospace:size=11", "FontAwesome:size=14" };
 static const char dmenufont[]       = "monospace:size=11";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeTabActive]  = { col_gray2, col_gray3,  col_gray2 },
-	[SchemeTabInactive]  = { col_gray1, col_gray3,  col_gray1 }
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3]      = {
+	/*               	    fg           bg           border   */
+	[SchemeNorm] 		= { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  		= { selfgcolor, selbgcolor, selbordercolor  },
+	[SchemeTabActive]  	= { normbordercolor, normfgcolor, normbordercolor},
+	[SchemeTabInactive]  	= { normbgcolor, normfgcolor, normbgcolor },
 };
 
 /* tagging */
@@ -73,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *brightness_up[] = { "brightness_up", NULL };
 static const char *brightness_down[] = { "brightness_down", NULL };
